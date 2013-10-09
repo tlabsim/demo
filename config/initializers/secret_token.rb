aec11f83f33735deaf5c979ca6887d33f4215828
tlabsim <im_tlabs@yahoo.com>
2013-10-09 12:19:09 +0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '713ef788b9d3b3f63d807c2310152169b4d6279330ef4b330ced34acfbab33dd8936d03c5a74cede080a4fd04695fb7c029e9fb812164f3b154e1f16f0d9b08a'
